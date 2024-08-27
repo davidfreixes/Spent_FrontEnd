72e386a2053de831ad7772a0b3ea76bf72e1c63d
David Freixes <dfis1996@gmail.com>
2024-08-27 11:07:47 +0200

    Update SpentApi.ts

--- src/api/SpentApi.ts
+++ src/api/SpentApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const SpentApi = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://considerate-manifestation-production.up.railway.app',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Methods': 'GET, POST, DELETE, PATCH'
