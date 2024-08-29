8759aa69d65b184e62ee0b2659fae7cf9c8d74ae
David Freixes <dfis1996@gmail.com>
2024-08-29 20:52:21 +0200

    Update SpentApi.ts

--- src/api/SpentApi.ts
+++ src/api/SpentApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const SpentApi = axios.create({
-    baseURL: 'https://spent-api.up.railway.app',
+    baseURL: 'https://nuclear-annalee-spentapi-e8d34daf.koyeb.app',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Methods': 'GET, POST, DELETE, PATCH'
