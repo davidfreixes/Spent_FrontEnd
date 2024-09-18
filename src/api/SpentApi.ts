c46bbaf63a8d5179a8638496830d197845060f99
David Freixes <dfis1996@gmail.com>
2024-09-18 19:48:35 +0200

    testing URL

--- src/api/SpentApi.ts
+++ src/api/SpentApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const SpentApi = axios.create({
-    baseURL: 'https://nuclear-annalee-spentapi-e8d34daf.koyeb.app/',
+    baseURL: 'https://nuclear-annalee-spentapi-e8d34daf.koyeb.app',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Methods': 'GET, POST, DELETE, PATCH'
