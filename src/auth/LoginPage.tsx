8670dd1da438c7f292cc0c81764890c2107f3997
David Freixes <dfis1996@gmail.com>
2024-09-21 19:31:36 +0200

    changing background image from login and register page

--- src/auth/LoginPage.tsx
+++ src/auth/LoginPage.tsx
@@ -51,7 +51,7 @@ export default function LoginPage() {
               ? t.palette.grey[50]
               : t.palette.grey[900],
           backgroundSize: "cover",
-          backgroundPosition: "left",
+          backgroundPosition: "center",
         }}
       />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
