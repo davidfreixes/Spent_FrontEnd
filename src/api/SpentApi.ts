0f56e8d2503d15e11f73e7fc500dd0d45a6f35c6
David Freixes <dfis1996@gmail.com>
2025-02-20 18:33:08 +0100

    fix url

--- src/api/SpentApi.ts
+++ src/api/SpentApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const SpentApi = axios.create({
-    baseURL: 'https://spent-alpha.vercel.app/',
+    baseURL: 'https://nuclear-annalee-spentapi-e8d34daf.koyeb.app',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Methods': 'GET, POST, DELETE, PATCH'
